1f1c6cf7f81d9a0fc4751175683414d595efe2c4
Benjamin Casey <benjamincasey@192-168-1-100.tpgi.com.au>
2024-06-10 05:05:37 +1000

    Added a main.cpp to test WeightedGraph.cpp

--- dijkstrasAlgorithm/WeightedGraph.h
+++ dijkstrasAlgorithm/WeightedGraph.h
@@ -21,3 +21,4 @@ public:
     void printGraph();
 };
 
+#endif
---
+++ dijkstrasAlgorithm/main.cpp
@@ -0,0 +1,15 @@
+#include "WeightedGraph.h"
+#include <iostream>
+
+int main() {
+    WeightedGraph g(4);
+
+    g.addEdge(0, 1, 10);
+    g.addEdge(0, 2, 20);
+    g.addEdge(1, 2, 30);
+    g.addEdge(2, 3, 40);
+
+    g.printGraph();
+
+    return 0;
+}
